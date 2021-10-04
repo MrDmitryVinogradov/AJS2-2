5ce24749810ebe8153bbaa163f7f9b7db2bec383
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-10-04 22:42:34 +0300

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
+
+  import Character from 'domain';
+
+  export { GameSavingData, readGameSaving, writeGameSaving }
